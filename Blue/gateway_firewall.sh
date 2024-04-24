af79288d24834f03455c3f9b71cd549f61ff47b1
Kyle Lastimosa <glitchkyle16@gmail.com>
2024-04-23 23:28:08 -0400

    Prevent SYN flood to gateway

--- Blue/gateway_firewall.sh
+++ Blue/gateway_firewall.sh
@@ -10,7 +10,10 @@ EXPERIMENTAL_IF="eth0"
 # Flush Tables
 sudo iptables --flush
 
-# Block SYN packets
+# Block SYN flood attack to gateway
+sudo iptables -A INPUT -p tcp ! --syn -m state --state NEW -j DROP
+
+# Block SYN flood attack to server
 sudo iptables -A FORWARD -p tcp ! --syn -m state --state NEW -j DROP
 
 echo "Firewall Active"
